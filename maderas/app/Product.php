9385c1f9aa537755021574d42709896eabf4778d
jazminValencia <jvalencia@powersoftec.com>
2020-06-19 13:16:13 -0600

    modulos y controller

---
+++ maderas/app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+    
+    protected $primarykey = "PK_products";
+    protected $fillable =['SKU','id_subcategory','name','description','price','avaible','status'];
+
+    public function Categories()
+    {
+        return $this->belongsTo('App\Subcategorie', 'id_subcategory', 'PK_subcategories');
+    }
+}
